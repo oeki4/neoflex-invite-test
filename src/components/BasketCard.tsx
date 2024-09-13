dd87d9dcdb32823e36b236e62d51b173ebbd3e11
Nikita-Minov <nikitachryackhov555@gmail.com>
2024-09-13 21:35:21 +0300

    Basket page and mini updates

--- src/components/BasketCard.tsx
+++ src/components/BasketCard.tsx
@@ -1,31 +1,37 @@
 import "../assets/scss/components/basket-card.scss";
 import Trash from "./Icons/Trash.tsx";
+import WhiteTrash from "./Icons/WhiteTrash.tsx";
 
 export default function BasketCard() {
   return (
     <div className="card">
       <div className="card__info">
-        <img
-          className="card__info-img"
-          src="https://i.imgur.com/Fpm86L9.png"
-          alt="photo"
-        />
+        <div className="card__img">
+          <img
+            className="card__img-inner"
+            src="https://i.imgur.com/Fpm86L9.png"
+            alt="photo"
+          />
+        </div>
         <div className="card__desc">
           <p className="card__desc-name">Apple BYZ S852I</p>
           <p className="price">2 927 ₽</p>
         </div>
-        <div className="switch">
-          <button className="switch__btn">&#8211;</button>
-          <span className="switch__value">1</span>
-          <button className="switch__btn">+</button>
+        <div className="card__switch-price">
+          <div className="switch">
+            <button className="switch__btn">&#8211;</button>
+            <span className="switch__value">1</span>
+            <button className="switch__btn">+</button>
+          </div>
+          <p className="price price--result">2 927 ₽</p>
         </div>
       </div>
-      <div className="card__result">
-        <span>
-          <Trash />
-        </span>
-        <p className="price price--result">2 927 ₽</p>
-      </div>
+      <button className="card__delete">
+        <WhiteTrash />
+      </button>
+      <span className="card__trash">
+        <Trash />
+      </span>
     </div>
   );
 }
--- src/pages/Basket.tsx
+++ src/pages/Basket.tsx
@@ -5,8 +5,17 @@ export default function Basket() {
   return (
     <section className="basket">
       <h2 className="basket__subtitle">Корзина</h2>
-      <div>
-        <BasketCard />
+      <div className="basket__inner">
+        <div className="basket__inner-cards">
+          <BasketCard />
+        </div>
+        <div className="order">
+          <div className="order__info">
+            <p className="order__info-text">Итого</p>
+            <p className="order__info-price">₽ 2 927</p>
+          </div>
+          <button className="order__btn">Перейти к оформлению</button>
+        </div>
       </div>
     </section>
   );
