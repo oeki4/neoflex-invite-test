874cee8afd7f81381b88f7dafe19be0843668da0
Nikita-Minov <nikitachryackhov555@gmail.com>
2025-04-01 23:14:15 +0300

    update libraries

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./assets/scss/main.scss";
 import "./assets/scss/global.scss";
 import { RouterProvider } from "react-router-dom";
 import routes from "./router/routes.ts";
